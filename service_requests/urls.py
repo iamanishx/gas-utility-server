6f0d90f9e8db57fdc1874c5d007e5977e3ee679c
iamanishx <manishbiswal754@gmail.com>
2025-01-19 23:37:17 +0530

    done

--- service_requests/urls.py
+++ service_requests/urls.py
@@ -11,10 +11,13 @@ urlpatterns = [
 '''
 from django.urls import path
 from . import views
+from django.shortcuts import redirect  # Import redirect function
+
 
 # app_name = 'service_requests'
 
 urlpatterns = [
+    path('', lambda request: redirect('list_service_requests')),  # Redirect root URL to service-requests
     path('new/', views.ServiceRequestCreateView.as_view(), name='create_service_request'),
     path('service-requests/', views.ServiceRequestListView.as_view(), name='list_service_requests'),
     path('<int:pk>/', views.ServiceRequestDetailView.as_view(), name='service_request_detail'),
